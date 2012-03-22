5ff5f1fc989d43c9b38bc4656afb936b5afb36e8
Christopher Petrilli <petrilli@amber.org>
2012-03-22 17:03:00 -0400

    Added masquerading capability.
    To use this capability, you provide the user you "want to be", what we call the
    proposed user, as the username, but then you provide a staff user's login and
    password, separated with a '/' in the password field.

--- emailusernames/backends.py
+++ emailusernames/backends.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User
 from emailusernames.utils import get_user
 
+MASQUERADE_SEPARATOR = '/'
 
 class EmailAuthBackend(object):
+    """Allow users to log in with their email address.
 
-    """Allow users to log in with their email address"""
+    It also allows a staff member to impersonate another user. This is done
+    by parsing the password field differently. If the user is unable to be
+    authenticated initially, the password field is split on a '/', and if found,
+    then the field will become a username and password combination to use."""
 
     supports_inactive_user = False
     supports_anonymous_user = False
@@ -12,9 +17,18 @@ class EmailAuthBackend(object):
 
     def authenticate(self, email=None, password=None):
         try:
-            user = get_user(email)
-            if user.check_password(password):
-                return user
+            proposed_user = get_user(email)
+            if proposed_user.check_password(password):
+                proposed_user.backend = "%s.%s" % (self.__module__, self.__class__.__name__)
+                return proposed_user
+            else:
+                # See if we can masquerade or not
+                if MASQUERADE_SEPARATOR in password:
+                    (masked_email, masked_password) = password.split(MASQUERADE_SEPARATOR, 1)
+                    real_user = self.authenticate(email=masked_email, password=masked_password)
+                    if real_user and real_user.is_staff:
+                        return proposed_user
+                return None
         except User.DoesNotExist:
             return None
 
